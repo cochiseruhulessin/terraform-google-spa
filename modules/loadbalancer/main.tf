a8f88be8388a91e3539afcfbd3a9235ed6e5e64e
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2022-09-19 03:06:07 +0200

    Use managed TLS certificate

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -6,78 +6,19 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-terraform {
-  required_providers {
-    acme = {
-      source = "vancluever/acme"
-      version = "2.6.0"
-    }
-    tls = {
-      version = "3.1.0"
-    }
-  }
-}
+terraform {}
 
-resource "tls_private_key" "acme" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
 
-resource "acme_registration" "default" {
-  account_key_pem = tls_private_key.acme.private_key_pem
-  email_address   = var.acme_email
-}
-
-resource "tls_private_key" "server" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-resource "tls_cert_request" "csr" {
-  depends_on = [tls_private_key.server]
-  key_algorithm   = "RSA"
-  private_key_pem = tls_private_key.server.private_key_pem
-  dns_names       = [var.acme_domain]
-
-  subject {
-    country             = "NL"
-    province            = "Gelderland"
-    locality            = "Wezep"
-    organization        = "Unimatrix One B.V."
-    organizational_unit = "PKI"
-    common_name         = var.acme_domain
-  }
-}
-
-resource "acme_certificate" "crt" {
-  depends_on              = [
-    acme_registration.default,
-    tls_private_key.acme,
-    tls_private_key.server
-  ]
-  account_key_pem         = tls_private_key.acme.private_key_pem
-  certificate_request_pem = tls_cert_request.csr.cert_request_pem
-  min_days_remaining      = 30
-  recursive_nameservers   = ["8.8.8.8:53"]
-
-  dns_challenge {
-    provider = "gcloud"
-
-    config = {
-      GCE_PROJECT = var.dns_project
-    }
-  }
-}
-
-resource "google_compute_ssl_certificate" "tls" {
-  depends_on  = [tls_private_key.server]
-  project     = var.project
-  name_prefix = var.name
-  private_key = tls_private_key.server.private_key_pem
-  certificate = "${acme_certificate.crt.certificate_pem}${acme_certificate.crt.issuer_pem}"
+resource "google_compute_managed_ssl_certificate" "tls" {
+  project = var.project 
+  name    = var.name
 
   lifecycle {
     create_before_destroy = true
+  }
+
+  managed {
+    domains = ["${var.acme_domain}."]
   }
 }
 
@@ -128,7 +69,7 @@ resource "google_compute_target_https_proxy" "default" {
   provider          = google
   name              = var.name
   url_map           = google_compute_url_map.urls.self_link
-  ssl_certificates  = [google_compute_ssl_certificate.tls.self_link]
+  ssl_certificates  = [google_compute_managed_ssl_certificate.tls.self_link]
 }
 
 resource "google_compute_global_address" "ipv4" {
